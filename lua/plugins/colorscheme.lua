bc3da79ecdbf8c9b55989a7aa5bf70186462416a
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-09-19 02:27:20 +0300

    make background transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({ transparent_background = true })
+		vim.cmd.colorscheme("catppuccin")
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff" })
+	end,
 }
