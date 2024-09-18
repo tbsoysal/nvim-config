a6b50845c374a67dfeaae898fb71b54fcb580370
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-09-19 00:22:11 +0300

    make neotree default for navgation

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,9 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+    filesystem = { hijack_netrw_behavior = "open_current" }
+    })
     vim.keymap.set('n', '<leader>e', ':Neotree right toggle<CR>', {silent=true})
   end
 }
